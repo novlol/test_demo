2a6a5866cc8e9165e807cee5bf285baa02d22dfc
jiaboran <jiaborana@163.com>
2023-03-10 10:01:05 +0800

    test

--- test_cases/case_demo/test_api.py
+++ test_cases/case_demo/test_api.py
@@ -11,55 +11,55 @@ class TestAPI:
             assert response.status_code == 200
 
     @allure.title("test_2")
-    def test_1(self, base_url, session):
+    def test_2(self, base_url, session):
         with allure.step('请求百度'):
             response = session.get(base_url)
             assert response.status_code == 200
 
     @allure.title("test_3")
-    def test_1(self, base_url, session):
+    def test_3(self, base_url, session):
         with allure.step('请求百度'):
             response = session.get(base_url)
             assert response.status_code == 200
 
     @allure.title("test_4")
-    def test_1(self, base_url, session):
+    def test_4(self, base_url, session):
         with allure.step('请求百度'):
             response = session.get(base_url)
             assert response.status_code == 200
 
     @allure.title("test_5")
-    def test_1(self, base_url, session):
+    def test_5(self, base_url, session):
         with allure.step('请求百度'):
             response = session.get(base_url)
             assert response.status_code == 200
 
     @allure.title("test_6")
-    def test_1(self, base_url, session):
+    def test_6(self, base_url, session):
         with allure.step('请求百度'):
             response = session.get(base_url)
             assert response.status_code == 200
 
     @allure.title("test_7")
-    def test_1(self, base_url, session):
+    def test_7(self, base_url, session):
         with allure.step('请求百度'):
             response = session.get(base_url)
             assert response.status_code == 200
 
     @allure.title("test_8")
-    def test_1(self, base_url, session):
+    def test_8(self, base_url, session):
         with allure.step('请求百度'):
             response = session.get(base_url)
             assert response.status_code == 200
 
     @allure.title("test_9")
-    def test_1(self, base_url, session):
+    def test_9(self, base_url, session):
         with allure.step('请求百度'):
             response = session.get(base_url)
             assert response.status_code == 200
 
     @allure.title("test_10")
-    def test_1(self, base_url, session):
+    def test_10(self, base_url, session):
         with allure.step('请求百度'):
             response = session.get(base_url)
             assert response.status_code == 200
